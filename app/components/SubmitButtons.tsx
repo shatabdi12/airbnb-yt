234b25b1c14193fd52598fd108f9d27f86b828e2
shatabdi12 <shatabdi.dey12@gmail.com>
2024-07-31 22:28:51 +0200

    feat: Add loader

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -2,15 +2,21 @@
 
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 export function CreationSubmit() {
     const { pending } = useFormStatus();
     return (
         <>
         {pending ? (
-            <Button type="submit" size="lg">Next</Button>
+            <Button disabled size="lg">
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+            </Button>
         ): (
-            <Button type="submit" size="lg">Next</Button>
+            <Button type="submit" size="lg">
+                Next
+            </Button>
         )}
         </>
     )
